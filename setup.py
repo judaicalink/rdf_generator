27b4becb6f90d3501349733622fbacff1bddd57b
Benjamin Schnabel <Benjamin-777@gmx.de>
2025-01-28 17:48:25 +0100

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='rdf-generator',
-    version='0.1.1',
-    packages=find_packages(),
-    install_requires=[
-        'rdflib',
-        'pandas',
-        'beautifulsoup4',
-        'pymysql',
-        'PyPDF2'
-    ],
+    version='0.1.2',
     entry_points={
         'console_scripts': [
             'rdfgen=rdf_generator.cli:main',
@@ -19,6 +14,23 @@ setup(
     author="Benjamin Schnabel",
     author_email="b.schnabel@hs-mannheim.de",
     description="A library for generating RDF datasets from various data sources",
-    long_description=open('README.md').read(),
-    url="https://github.com/judaicalink/rdf-generator",
+    long_description=long_description,  # Load the README as the long description
+    long_description_content_type="text/markdown",  # Specify Markdown format
+    url="https://github.com/judaicalink/rdf_generator",
+    packages=["rdf-generator"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "rdflib",
+        "pandas",
+        "requests",
+        "beautifulsoup4",
+        "PyPDF2",
+        "mysql-connector-python",
+        "psycopg2",
+    ],
 )
